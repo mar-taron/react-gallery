2cc72882028f542e0ca37e6494f54a9d97056aa1
mar-taron <mtaron91@gmail.com>
2018-10-27 18:05:29 +0400

    Image gallery with selection

---
+++ src/helpers/prepareArray.js
@@ -0,0 +1,23 @@
+ // {
+ //            src: "https://c4.staticflickr.com/8/7476/28973628875_069e938525_b.jpg",
+ //            thumbnail: "https://c4.staticflickr.com/8/7476/28973628875_069e938525_n.jpg",
+ //            thumbnailWidth: 320,
+ //            thumbnailHeight: 213,
+ //            caption: "Time to Think (Tom Eversley - isorepublic.com)"
+ //        }
+
+export default function prepareArray(array) {
+	const prepatedArray = [];
+	cons item = {};
+	array.map((src)=>{
+		
+        item = {
+        	src,
+
+        }
+		console.log(src);
+	})
+	
+	
+	return prepatedArray;	
+}
